75e1ef103e88b990360192ef1743e8d21b88a858
Ignas Kavaliauskas <ignas.kavaliauskas@gmx.de>
2019-03-27 00:10:24 +0100

    you can define update speed in .start(..) function

--- yt-sub-counter/js/script.js
+++ yt-sub-counter/js/script.js
@@ -1,11 +1,11 @@
-const CHANNEL_ONE = "UCiye1pHfrRQjIM-93Oe9wNw"; //potato analytics
+const CHANNEL_ONE = "UC-lHJZR3Gqxm24_Vd_AJ5Yw"; //pewdiepie
 const CHANNEL_TWO = "UCq-Fj5jknLsUf-MWSy4_brA"; //t-series
 
 
 $(() => {
     // Init app
     new YouTubeData()
-    .addChannel(new Channel(CHANNEL_ONE, 'one'))
-    .addChannel(new Channel(CHANNEL_TWO, 'two'))
-    .start();
+        .addChannel(new Channel(CHANNEL_ONE, 'one'))
+        .addChannel(new Channel(CHANNEL_TWO, 'two'))
+        .start();
 });
